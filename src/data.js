e7265e47ae37b69d6fbdc9c810474dbeb13c82cb
solenwa <solen.wanono@gmail.com>
2024-09-09 15:33:04 +0200

    Template done, Frontend ongoing

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export default {
+  products: [
+    {
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpeg',
+      price: 60,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      name: 'Fit Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpeg',
+      price: 50,
+      brand: 'Nike',
+      rating: 4.2,
+      numReviews: 10,
+    },
+    {
+      name: 'Best Pants',
+      category: 'Pants',
+      image: '/images/d1.jpeg',
+      price: 55,
+      brand: 'Nike',
+      rating: 4.7,
+      numReviews: 10,
+    },
+  ],
+};
